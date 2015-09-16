c4a3064c2673acbdec8ecf7b5850e9314277cad5
Admin123 <cuizhennan@6clue.com>
2015-09-16 19:04:59 +0800

    init dubboclient

---
+++ dubboclient/src/main/java/demo/action/ChatAction.java
@@ -0,0 +1,8 @@
+package demo.action;
+
+/**
+ * @author zhennan-cui @date 2015年9月16日
+ */
+public class ChatAction {
+
+}
---
+++ dubboclient/src/main/java/demo/main/Main.java
@@ -0,0 +1,8 @@
+package demo.main;
+
+/**
+ * @author zhennan-cui @date 2015年9月16日
+ */
+public class Main {
+
+}
