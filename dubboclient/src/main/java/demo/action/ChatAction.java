c56b3821e811008d56c7c91fcd81c5c19de7f7b8
Admin123 <cuizhennan@6clue.com>
2015-09-18 12:19:10 +0800

    finish dubbo test

--- dubboclient/src/main/java/demo/action/ChatAction.java
+++ dubboclient/src/main/java/demo/action/ChatAction.java
@@ -1,8 +1,29 @@
 package demo.action;
 
+import java.util.Calendar;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import demo.service.IDemoService;
+
 /**
  * @author zhennan-cui @date 2015年9月16日
  */
 public class ChatAction {
 
+    private ClassPathXmlApplicationContext context;
+
+    //    @Resource
+    //    IDemoService demoService;
+
+    public void sayHello() {
+        context = new ClassPathXmlApplicationContext(new String[] { "applicationConsumer.xml" });
+        context.start();
+
+        IDemoService demoService = (IDemoService) context.getBean("demoService");
+
+        System.out.println("client:"
+                + demoService.sayHello("Morning" + "1:==> " + Calendar.getInstance().getTime())
+                + " ==>3:" + Calendar.getInstance().getTime());
+    }
 }
